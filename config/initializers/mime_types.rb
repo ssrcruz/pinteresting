61104dd686475c017791a9832a36d850c11cbdf9
Ruben Cruz <ssrcruz@gmail.com>
2014-10-02 16:57:55 -0400

    Added jQuery masonry

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,10 @@
+module Paperclip 
+  class MediaTypeSpoofDetector 
+    def spoofed? 
+      false 
+    end 
+  end 
+end
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
